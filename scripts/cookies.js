74bbf023a1de1c32516d785747f25166a5261e01
Yukta Sagar <sagaryukta@gmail.com>
2015-12-08 18:17:27 -0500

    cookies

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,32 +1,30 @@
-function writeCookie(cookieName, cookieValue, expireHours, path, domain){
-	var date =  new Date();
-date.setHours(date.getHours + expireHours);
-document.cookie = cookieName + '=' + cookieValue + '; expires=' + date + '; path=' + path + '; domain=' + domain;
-}
- 
-function readCookie(cookieName){
-	var textArray = document.cookie.split(';');
-		for(var i = 0; i < textArray.length; i++){
-	var textPiece = textArray[i]; 
-		while(textPiece(0)==' ') textPiece = textPiece.substring(1,textPiece.length);
-		if (textPiece.indexOf(cookieName)== 0)
-			return textPiece.substring(cookieName.length,c.length);
-	}
-}
-var summerSlam = function(){
+var summerSlam=function(){
     return{
-		formValidate: function(myform){
-			alert("hello there");
-			return false;
-			var requiredFields = ["name", "email", "phn" ]
-			for(var x = 0; x < form.elements.length; x++){
-				if(requiredFields.indexOf(form.elements[x].name)>-1){	
-				if((form.elements[x].value.length <=0) || (form.elements[x].value == " ") ){
+		formValidate: function(form){
+			var requiredFields=["name", "email", "phn" ]
+			var storedValues="";
+			for(var x=0; x<form.elements.length; x++){
+				if(requiredFields.indexOf(form.elements[x].id)>-1){	
+					if((form.elements[x].value.length<=0) || (form.elements[x].value == " ") ){
 					form.elements[x].focus()
+					console.log(form.elements[x].value.length)
 					alert("please complete all required fields")
-					return false;}
+					return false;
+					}
+				}
+				if(form.elements[x].id{
+					storedValues+= form.elements[x].id+"="+form.elements[x].value+",";
 				}
 			}
+			this.createCookie("sslam", storedValues, 30)
+			return false;
+		},
+		createCookie: function(cookie_name,cookie_value, daysToExpire){
+			var expirationDate=new Date();
+			expirationDate.setDate(expirationDate.getDate()+daysToExpire);
+			cookie_value+= ";expires="+expirationDate;
+			document.cookie=cookie_name+"="+cookie_value;
 		}
 	};
 }();
+	
