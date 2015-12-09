4157440fcc1df29389655bee1d5247b4dc373eb4
Yukta Sagar <sagaryukta@gmail.com>
2015-12-08 20:38:55 -0500

    works in ie

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,29 +1,30 @@
-var summerSlam=function(){
-    return{
-		formValidate: function(form){
-			var requiredFields=["name", "email", "phn"]
-			var storedValues="";
-			for(var x=0; x<form.elements.length; x++){
-				if(requiredFields.indexOf(form.elements[x].id)>-1){	
-					if((form.elements[x].value.length<=0) || (form.elements[x].value == " ") ){
-					form.elements[x].focus()
-					console.log(form.elements[x].value.length)
-					alert("please complete all required fields")
-					return false;
-					}
-				}
-				if(form.elements[x].id{
-					storedValues+= form.elements[x].id+"="+form.elements[x].value+",";
-				}
-			}
-			this.createCookie("sslam", storedValues, 30)
-			return false;
-		},
-		createCookie: function(cookie_name,cookie_value, daysToExpire){
-			var expirationDate=new Date();
-			expirationDate.setDate(expirationDate.getDate()+daysToExpire);
-			cookie_value+= ";expires="+expirationDate;
-			document.cookie=cookie_name+"="+cookie_value;
-		}
-	};
-}();
+function setCookie(cookie_name,cookie_value, daysToExpire){
+	var expirationDate=new Date();
+	expirationDate.setTime(expirationDate.getTime()+(daysToExpire*24*60*60*1000));
+	var expires = "expires="+expirationDate.toGMTString();
+	document.cookie=cookie_name+"="+cookie_value;
+}
+function getCookie(cookie_name) {
+    var name = cookie_name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user=getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+           setCookie("username", user, 30);
+       }
+    }
+}
