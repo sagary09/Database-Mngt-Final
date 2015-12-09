3a47c5432f7c05b544989739cf0f9b66e5a19879
Yukta Sagar <sagaryukta@gmail.com>
2015-12-09 18:08:52 -0500

    formValidate
    formvalidate works but can't store data in cookie

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -11,7 +11,7 @@ var x=document.forms["myForm"]["username"].value;
 	if(form.elements[x].id){
 		storedValues+= form.elements[x].id+"="+form.elements[x].value+",";
 	}
-	this.createCookie("cookie_name", storedValues, 30)
+	this.createCookie("sslam", storedValues, 30)
 	return false;
 }
 function createCookie(cookie_name, cookie_value, daysToExpire){
@@ -19,6 +19,8 @@ function createCookie(cookie_name, cookie_value, daysToExpire){
 	expirationDate.setTime(expirationDate.getTime()+(daysToExpire*24*60*60*1000));
 	var expires = "expires="+expirationDate.toGMTString();
 	document.cookie=cookie_name+"="+cookie_value;
+		if(username = getCookie("username")) document.getElementById("username").value = username;
+		if(password = getCookie("password")) document.getElementById("password").value = password;
 }
 function getCookie(cookie_name) {
     var name = cookie_name + "=";
@@ -28,15 +30,11 @@ function getCookie(cookie_name) {
         while (c.charAt(0)==' ') c = c.substring(1);
         if (c.indexOf(name) == 0) {
             return c.substring(name.length, c.length);
+			
         }
     }
+    return "";
 }
-function deleteCookie(cookie_name){
-    document.cookie=name + "=null; path=/; expires=" + expired.toGMTString();
-}
-function storeValues(myForm){
-	setCookie("username", myForm.username.value);
-	setCookie("password", myForm.password.value);
-}
+  
 
 
