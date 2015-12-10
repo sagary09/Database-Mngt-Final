23eaf4dd8ad11c45c014883498304bbb5773d54c
Yukta Sagar <sagaryukta@gmail.com>
2015-12-10 17:18:09 -0500

    username input saved
    only in IE though

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -8,15 +8,15 @@ var x=document.forms["myForm"]["username"].value;
 		alert("Welcome!");
 	}
 }
-var remembervalues_days=60 //Remember text input values for how many days?
-var dyncookiename=encodeURI(window.location.pathname) //Name of cookie. Changes depending on the page.
+var remembervalues_days=60
+var dyncookiename=encodeURI(window.location.pathname) 
 
 var recallinput=function(){
 var cookienamevalue=rememberinput.getCookie(dyncookiename).split("##")
 for (var i=0; i<cookienamevalue.length; i++){
 var cookiename=cookienamevalue[i].split("#")[0]
 var cookievalue=cookienamevalue[i].split("#")[1]
-if (document.getElementById(cookiename)!=null && document.getElementById(cookiename).type=="text") //if this text field has a stored value
+if (document.getElementById(cookiename)!=null && document.getElementById(cookiename).type=="text")
 document.getElementById(cookiename).value=decodeURI(cookievalue)
 }
 }
@@ -24,22 +24,21 @@ document.getElementById(cookiename).value=decodeURI(cookievalue)
 var rememberinput=function(){
 var rememberit=""
 for (var i=0; i<arguments.length; i++){
-if (document.getElementById(arguments[i]).type=="text" && document.getElementById(arguments[i]).value!="") //if this is a form text INPUT and not empty
+if (document.getElementById(arguments[i]).type=="text" && document.getElementById(arguments[i]).value!="") 
 rememberit+=arguments[i]+"#"+encodeURI(document.getElementById(arguments[i]).value)+"##"
 }
 rememberinput.setCookie(dyncookiename, rememberit, remembervalues_days)
 }
 
-rememberinput.getCookie=function(Name){ //get cookie value
-var re=new RegExp(Name+"=[^;]+", "i"); //construct RE to search for target name/value pair
-if (document.cookie.match(re)) //if cookie found
-return document.cookie.match(re)[0].split("=")[1] //return its value
+rememberinput.getCookie=function(Name){ 
+var re=new RegExp(Name+"=[^;]+", "i"); 
+if (document.cookie.match(re)) 
+return document.cookie.match(re)[0].split("=")[1] 
 return ""
 }
 
-rememberinput.setCookie=function(name, value, days){ //set cookie value
+rememberinput.setCookie=function(name, value, days){ 
 var expireDate = new Date()
-//set "expstring" to either future or past date, to set or delete cookie, respectively
 var expstring=expireDate.setDate(expireDate.getDate()+parseInt(days))
 document.cookie = name+"="+value+"; expires="+expireDate.toGMTString()+";";
 }
